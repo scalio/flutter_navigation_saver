2846fb4c36f1508e8fc6bf2b25e80487992fa2e1
mig35 <mig35@mig35.com>
2019-11-21 14:51:34 +0700

    initial commit with main navigation saver logic code

---
+++ navigation_saver/lib/navigation_saver.dart
@@ -0,0 +1,226 @@
+library navigation_saver;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pedantic/pedantic.dart';
+
+typedef NavigationRoutesSaver = Future<void> Function(Iterable<RouteSettings> activeRoutes);
+typedef NavigationRoutesRestorer = Future<Iterable<RouteSettings>> Function();
+
+typedef WidgetRouteGenerator = Route<dynamic> Function(RouteSettings routeSettings);
+typedef NavigationSaverRouteFactory = Route<dynamic> Function(
+  RouteSettings settings,
+  String routeName,
+  Object routeArguments, {
+  NextPageInfo nextPageInfo,
+});
+
+class NavigationSaver extends NavigatorObserver {
+  NavigationSaver(
+    this._navigationRoutesSaver,
+    this._navigationRoutesRestorer, {
+    String defaultNavigationRoute,
+  })  : assert(null != _navigationRoutesSaver, 'navigationRoutesSaver should not ne null'),
+        assert(null != _navigationRoutesRestorer, 'navigationRoutesRestorer should not ne null'),
+        this._defaultNavigationRoute = defaultNavigationRoute ?? Navigator.defaultRouteName;
+
+  static final String restoreRouteName = 'navigationSaverRestore';
+
+  final NavigationRoutesSaver _navigationRoutesSaver;
+  final NavigationRoutesRestorer _navigationRoutesRestorer;
+
+  final String _defaultNavigationRoute;
+
+  int _routesVersion = 0;
+  final List<Route<dynamic>> _activeRoutes = <Route<dynamic>>[];
+
+  Future<void> restorePreviousRoutes(BuildContext context) async {
+    final Iterable<RouteSettings> routeSettings = await _navigationRoutesRestorer();
+    if (null == routeSettings) {
+      throw ArgumentError.notNull('routeSettings');
+    }
+
+    _restoreRoutesInternal(context, routeSettings.toList());
+  }
+
+  Route<dynamic> onGenerateRoute(
+    RouteSettings routeSettings,
+    NavigationSaverRouteFactory routeFactory,
+  ) {
+    if (routeSettings.name == NavigationSaver.restoreRouteName) {
+      return MaterialPageRoute(
+        builder: (BuildContext context) => NavigationRestorationWidget(
+          navigationSaver: this,
+          child: Container(
+            alignment: AlignmentDirectional.center,
+            color: Colors.blue,
+            child: CircularProgressIndicator(),
+          ),
+        ),
+        settings: routeSettings,
+      );
+    } else {
+      final routeArguments = routeSettings.arguments;
+      dynamic realArguments = routeArguments;
+      NextPageInfo nextPageInfo;
+      if (routeArguments is RestoredArguments) {
+        realArguments = routeArguments.arguments;
+        nextPageInfo = routeArguments.nextPageInfo;
+      }
+      return routeFactory(
+        routeSettings,
+        routeSettings.name,
+        realArguments,
+        nextPageInfo: nextPageInfo,
+      );
+    }
+  }
+
+  @override
+  void didReplace({Route<dynamic> newRoute, Route<dynamic> oldRoute}) {
+    _activeRoutes.remove(oldRoute);
+    _activeRoutes.add(newRoute);
+    _routesVersion++;
+    _saveRoutes();
+  }
+
+  @override
+  void didRemove(Route<dynamic> route, Route<dynamic> previousRoute) {
+    _activeRoutes.remove(route);
+    _routesVersion++;
+    _saveRoutes();
+  }
+
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
+    _activeRoutes.remove(route);
+    _routesVersion++;
+    _saveRoutes();
+  }
+
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
+    _activeRoutes.add(route);
+    _routesVersion++;
+    _saveRoutes();
+  }
+
+  void _saveRoutes() {
+    final int localRoutesVersion = _routesVersion;
+    final List<RouteSettings> localActiveRouteSettings = _activeRoutes
+        .map((Route route) => route.settings)
+        .where((RouteSettings settings) => settings.name != restoreRouteName)
+        .map((RouteSettings settings) {
+      if (settings.arguments is RestoredArguments) {
+        return RouteSettings(
+          name: settings.name,
+          isInitialRoute: settings.isInitialRoute,
+          arguments: (settings.arguments as RestoredArguments).arguments,
+        );
+      } else {
+        return settings;
+      }
+    }).toList();
+    Future(() async {
+      await Future.delayed(Duration(milliseconds: 10));
+      if (localRoutesVersion == _routesVersion) {
+        await _navigationRoutesSaver(localActiveRouteSettings);
+      }
+    });
+  }
+
+  void _restoreRoutesInternal(BuildContext context, List<RouteSettings> routeSettings) {
+    final NavigatorState navigator = Navigator.of(context);
+    while (navigator.canPop()) {
+      navigator.pop();
+    }
+    if (routeSettings.isEmpty) {
+      unawaited(navigator.pushReplacementNamed(_defaultNavigationRoute));
+    } else {
+      var prevRouteCompleter;
+      for (int i = 0; i < routeSettings.length; ++i) {
+        final RouteSettings routeSetting = routeSettings[i];
+        final RouteSettings nextRouteSetting =
+            (i < routeSettings.length - 1) ? routeSettings[i + 1] : null;
+        Completer currentRouteCompleter;
+        if (nextRouteSetting != null) {
+          currentRouteCompleter = Completer();
+        }
+        final RestoredArguments arguments = RestoredArguments(
+          null == nextRouteSetting
+              ? null
+              : NextPageInfo(nextRouteSetting.name, currentRouteCompleter.future),
+          routeSetting.arguments,
+        );
+        if (i == 0) {
+          navigator.pushReplacementNamed(routeSetting.name, arguments: arguments);
+        } else {
+          _waitForTheResultAndPublishAsLost(
+            () => navigator.pushNamed(routeSetting.name, arguments: arguments),
+            prevRouteCompleter,
+          );
+        }
+        if (null != currentRouteCompleter) {
+          prevRouteCompleter = currentRouteCompleter;
+        }
+      }
+    }
+  }
+
+  Future<void> _waitForTheResultAndPublishAsLost(
+    Future<dynamic> Function() navigationAction,
+    Completer completer,
+  ) async {
+    final result = await navigationAction();
+    completer?.complete(result);
+  }
+}
+
+class NavigationRestorationWidget extends StatefulWidget {
+  NavigationRestorationWidget({
+    @required this.navigationSaver,
+    @required this.child,
+  });
+
+  final NavigationSaver navigationSaver;
+  final Widget child;
+
+  @override
+  State<StatefulWidget> createState() => _NavigationRestorationState();
+}
+
+@immutable
+class RestoredArguments {
+  const RestoredArguments(
+    this.nextPageInfo,
+    this.arguments,
+  );
+
+  final NextPageInfo nextPageInfo;
+  final Object arguments;
+}
+
+@immutable
+class NextPageInfo {
+  const NextPageInfo(
+    this.routeName,
+    this.resultFuture,
+  );
+
+  final String routeName;
+  final Future resultFuture;
+}
+
+class _NavigationRestorationState extends State<NavigationRestorationWidget> {
+  @override
+  void initState() {
+    super.initState();
+
+    widget.navigationSaver.restorePreviousRoutes(context);
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+}
---
+++ navigation_saver/test/navigation_saver_test.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:navigation_saver/navigation_saver.dart';
+
+void main() {
+  testWidgets(
+    'initially we restore nothing',
+    (WidgetTester tester) async {
+      // Setup in memory navigation saver
+      Iterable<RouteSettings> savedRoutes = <RouteSettings>[];
+      final NavigationSaver navigationSaver = NavigationSaver(
+        (Iterable<RouteSettings> activeRoutes) async {
+          savedRoutes = activeRoutes;
+        },
+        () async => savedRoutes,
+      );
+
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(_MyApp(navigationSaver));
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+    },
+  );
+
+  testWidgets(
+    'if we push new widget then it is pushed',
+    (WidgetTester tester) async {
+      // Setup in memory navigation saver
+      Iterable<RouteSettings> savedRoutes = <RouteSettings>[];
+      final NavigationSaver navigationSaver = NavigationSaver(
+        (Iterable<RouteSettings> activeRoutes) async {
+          savedRoutes = activeRoutes;
+        },
+        () async => savedRoutes,
+      );
+
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(_MyApp(navigationSaver));
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter has incremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'if we push new widget and restore it - same widgets are visible',
+    (WidgetTester tester) async {
+      // Setup in memory navigation saver
+      Iterable<RouteSettings> savedRoutes = <RouteSettings>[];
+      final NavigationSaver navigationSaver = NavigationSaver(
+        (Iterable<RouteSettings> activeRoutes) async {
+          savedRoutes = activeRoutes;
+        },
+        () async => savedRoutes,
+      );
+
+      // Build our app and trigger a frame.
+      final _MyApp myApp = _MyApp(navigationSaver);
+      await tester.pumpWidget(myApp);
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter has incremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+
+      // Restore current navigation stack. Because it calls with an existing stack - nothing should change
+      await navigationSaver.restorePreviousRoutes(myApp._latestContext);
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter still incremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'if we push new widget, lock saving, kill back stack - it is restored',
+    (WidgetTester tester) async {
+      // Setup in memory navigation saver
+      bool saveRoutes = true;
+      Iterable<RouteSettings> savedRoutes = <RouteSettings>[];
+      final NavigationSaver navigationSaver = NavigationSaver(
+        (Iterable<RouteSettings> activeRoutes) async {
+          if (saveRoutes) {
+            savedRoutes = activeRoutes;
+          }
+        },
+        () async => savedRoutes,
+      );
+
+      // Build our app and trigger a frame.
+      final _MyApp myApp = _MyApp(navigationSaver);
+      await tester.pumpWidget(myApp);
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter has incremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+
+      // Disable stack saving logic. So we freeze it with 2 widgets inside
+      saveRoutes = false;
+
+      // Drop one widget
+      Navigator.of(myApp._latestContext).pop();
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+
+      // Restore old widget tree
+      await navigationSaver.restorePreviousRoutes(myApp._latestContext);
+      await tester.pump();
+      await tester.pump(Duration(seconds: 1));
+
+      // Verify that our counter still incremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+    },
+  );
+}
+
+// ignore: must_be_immutable
+class _MyApp extends StatelessWidget {
+  _MyApp(this._navigationSaver);
+
+  final NavigationSaver _navigationSaver;
+  BuildContext _latestContext;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      initialRoute: NavigationSaver.restoreRouteName,
+      onGenerateRoute: (RouteSettings routeSettings) => _navigationSaver.onGenerateRoute(
+        routeSettings,
+        (
+          RouteSettings settings,
+          String routeName,
+          Object routeArguments, {
+          NextPageInfo nextPageInfo,
+        }) =>
+            MaterialPageRoute(
+          builder: (BuildContext context) {
+            _latestContext = _latestContext ?? context;
+            final int currentDeepIndex = (routeArguments as int) ?? 0;
+            return Scaffold(
+              body: Column(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () => Navigator.of(context).pushNamed(
+                      '/next',
+                      arguments: currentDeepIndex + 1,
+                    ),
+                    icon: Icon(Icons.add),
+                  ),
+                  Text(currentDeepIndex.toString()),
+                ],
+              ),
+            );
+          },
+          settings: routeSettings,
+        ),
+      ),
+      navigatorObservers: [_navigationSaver],
+    );
+  }
+}
