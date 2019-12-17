35f67ef9475a9799e9898e4a1081016532bed865
mig35 <mig35@mig35.com>
2019-12-17 20:46:29 +0700

    shared preferences update

--- shared_pref_navigation_saver/lib/shared_pref_navigation_saver.dart
+++ shared_pref_navigation_saver/lib/shared_pref_navigation_saver.dart
@@ -20,41 +20,33 @@ class SharedPrefNavigationSaver extends NavigationSaver {
     String defaultNavigationRoute,
   }) : super(
           (Iterable<RouteSettings> activeRoutes) async {
-            try {
-              if (activeRoutes.isEmpty) return;
+            if (activeRoutes.isEmpty) return;
 
-              final SharedPreferences prefs =
-                  await SharedPreferences.getInstance();
-              await prefs.setString(
-                  _keyRoutes, await routesToStringConverter(activeRoutes));
-              await prefs.setInt(
-                  _keySaveTime, DateTime.now().toUtc().millisecondsSinceEpoch);
-            } catch (e) {
-              print(e);
-            }
+            final SharedPreferences prefs =
+                await SharedPreferences.getInstance();
+            await prefs.setString(
+                _keyRoutes, await routesToStringConverter(activeRoutes));
+            await prefs.setInt(
+                _keySaveTime, DateTime.now().toUtc().millisecondsSinceEpoch);
           },
           () async {
-            try {
-              final SharedPreferences prefs =
-                  await SharedPreferences.getInstance();
-              final int lastSavedTimeInMillis = prefs.getInt(_keySaveTime);
+            final SharedPreferences prefs =
+                await SharedPreferences.getInstance();
+            final int lastSavedTimeInMillis = prefs.getInt(_keySaveTime);
 
-              final DateTime nowTime = DateTime.now().toUtc();
-              DateTime lastSavedTime = nowTime;
-              if (null != lastSavedTimeInMillis) {
-                lastSavedTime =
-                    DateTime.fromMillisecondsSinceEpoch(lastSavedTimeInMillis);
-              }
-              final Duration diffTime = nowTime.difference(lastSavedTime);
-              if (null == maximumDurationBetweenRestoration ||
-                  diffTime < maximumDurationBetweenRestoration) {
-                final String routesAsString = prefs.getString(_keyRoutes);
-                if (null == routesAsString || routesAsString.isEmpty)
-                  return <RouteSettings>[];
-                return await stringToRoutesConverter(routesAsString);
-              }
-            } catch (e) {
-              print(e);
+            final DateTime nowTime = DateTime.now().toUtc();
+            DateTime lastSavedTime = nowTime;
+            if (null != lastSavedTimeInMillis) {
+              lastSavedTime =
+                  DateTime.fromMillisecondsSinceEpoch(lastSavedTimeInMillis);
+            }
+            final Duration diffTime = nowTime.difference(lastSavedTime);
+            if (null == maximumDurationBetweenRestoration ||
+                diffTime < maximumDurationBetweenRestoration) {
+              final String routesAsString = prefs.getString(_keyRoutes);
+              if (null == routesAsString || routesAsString.isEmpty)
+                return <RouteSettings>[];
+              return await stringToRoutesConverter(routesAsString);
             }
             return <RouteSettings>[];
           },
